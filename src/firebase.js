76e1f59391f4825c9a0f721a847ba408b3b0ba42
juanwmedia <juan@wmedia.es>
2020-12-29 11:14:24 +0100

    Creación del proyecto

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD03SCXkk8R3eajStLvKrDagpDez5KLwbk",
+  authDomain: "ideaboxlive.firebaseapp.com",
+  projectId: "ideaboxlive",
+  storageBucket: "ideaboxlive.appspot.com",
+  messagingSenderId: "319287508716",
+  appId: "1:319287508716:web:d3235408fb09055c7f6fc9"
+};
+
+firebase.initializeApp(firebaseConfig);
